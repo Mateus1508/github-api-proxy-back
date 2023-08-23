3d15250a0439fbb2442415a5912e4a3b1628f87e
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-08-23 17:08:09 -0300

    feat: implementing cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const server = express();
+
+server.use(cors());
+
 server.use(express.json());
 server.use(router);
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running!`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
